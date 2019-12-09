50f94522542ae70d4db681f5c9fe95a15ef4becb
varnittewari <vxt6823@rit.edu>
2019-12-09 00:14:12 -0500

    started second

--- productExceptSelf.java
+++ productExceptSelf.java
@@ -1,6 +1,17 @@
+/* Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division? */
+
 public class productExceptSelf{
 
-    public static void main(){
+    public static void main(String[] args){
+        int[] nums = {1, 2, 3, 4, 5};
+        System.out.println(prodExcept(nums));
+    }
+
+    public static int[] prodExcept(int[] nums){
         
     }
 }
