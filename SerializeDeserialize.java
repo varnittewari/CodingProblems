8ec68af296b755cbc2e378311e13a9370a0068d7
varnittewari <vxt6823@rit.edu>
2019-12-09 17:14:33 -0500

    ran first missing positive

--- SerializeDeserialize.java
+++ SerializeDeserialize.java
@@ -1,3 +1,6 @@
+/*Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, 
+and deserialize(s), which deserializes the string back into the tree. */
+
 public class codec {
 
     public static void main(String[] args){
---
+++ firstMissingPositive.java
@@ -0,0 +1,48 @@
+/* Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3. */
+
+public class solution {
+
+    public static void main(String[] args){
+        int[] arg = {3,4,-1,-1};
+        int a = firstMissingPositive(arg);
+        System.out.println(a);
+    }
+
+    public static int firstMissingPositive(int[] nums) {
+        if (nums.length==0)
+            return 1;
+        int n =nums.length;
+        int i=0;
+        while (i<n){
+            if (nums[i]==i+1)
+                i++;
+            else{
+                if (nums[i]>n || nums[i]<=0)
+                    i++;
+                else{
+                    if (nums[nums[i]-1]!=nums[i])
+                        swap(nums, i, nums[i]-1);
+                    else 
+                        i++;
+                }
+            }
+        }
+        
+        int max = 0;
+        for (i=0; i<nums.length; i++){
+            if (nums[i]!=i+1)
+                return i+1;
+            if (nums[i]>max)
+                max = nums[i];
+        }
+        return max+1;
+    }
+    
+    public static void swap(int[] nums, int m, int n){
+        int temp = nums[m];
+        nums[m] = nums[n];
+        nums[n] = temp;
+    }
+}
