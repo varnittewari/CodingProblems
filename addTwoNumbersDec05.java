226d629350136426a91a07cca9c4355bde84bcf8
varnittewari <vxt6823@rit.edu>
2019-12-09 00:08:57 -0500

    ran addTwo

--- addTwoNumbersDec05.java
+++ addTwoNumbersDec05.java
@@ -1,4 +1,5 @@
 import java.util.Map;
+import java.util.*;
 
 /* Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 
@@ -6,14 +7,14 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 Bonus: Can you do this in one pass? */
 public class addTwoNumbersDec05{
-    public static void main(){
+    public static void main(String[] args){
         int[] arg = {10,15,3,7};
         int k = 17;
         System.out.println(addTwoNum(arg, k));
     }
 
     public static boolean addTwoNum(int[] nums, int k){
-        Map<Integer, Integer> map = new HashMap<Integer,Integer>();
+        HashMap<Integer, Integer> map = new HashMap<Integer,Integer>();
         for (int i: nums){
             if (map.containsKey(k-i))
                 return true;
