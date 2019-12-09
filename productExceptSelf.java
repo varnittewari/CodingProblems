9431f98a9f067cfc0268a49446abdc94b32171ca
varnittewari <vxt6823@rit.edu>
2019-12-09 00:49:02 -0500

    ran prod

--- productExceptSelf.java
+++ productExceptSelf.java
@@ -12,6 +12,24 @@ public class productExceptSelf{
     }
 
     public static int[] prodExcept(int[] nums){
+        if (nums.length==0)
+            return nums;
+        int[] ans = new int[nums.length];
+        for (int i=0; i<nums.length; i++)
+            ans[i]=1;
         
+        int left=1;
+        for (int i=0; i< nums.length; i++){
+            ans[i] = ans[i]*left;
+            left = left*nums[i];
+        }
+        
+        int right=1;
+        for(int i=nums.length-1; i>=0;i--){
+            ans[i] = ans[i]*right;
+            System.out.println(ans[i]);
+            right = right*nums[i];
+        }
+        return ans;
     }
 }
