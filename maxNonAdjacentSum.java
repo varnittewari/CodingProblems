67b95170cdb83e52e0df8c4199658c61c8c2d498
varnittewari <vxt6823@rit.edu>
2019-12-14 13:05:19 -0500

    started manNonAdjacentSum

---
+++ maxNonAdjacentSum.java
@@ -0,0 +1,11 @@
+public class maxNonAdjacentSum{
+
+    public static void main(String[] args){
+        int[] nums = {2, 4, 6, 2, 5};
+        System.out.println(solution(nums));
+    }
+
+    public static int solution(int[] nums){
+        
+    }
+}
