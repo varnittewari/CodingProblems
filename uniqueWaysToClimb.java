b829f727b5a35e0f3b853073826a2d462e291bf2
varnittewari <vxt6823@rit.edu>
2019-12-16 19:01:03 -0500

    started uniqueWays commit

---
+++ uniqueWaysToClimb.java
@@ -0,0 +1,19 @@
+public class uniqueWaysToClimb{
+
+    public static void main(String[] args){
+        System.out.println(solution(4));
+        System.out.println(solution(4));
+        System.out.println(solution(4));
+    }
+
+    public static int solution(int n){
+        if (n==0) return 0;
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 2;
+        for (int i=2;i<n; i++){
+            arr[i] = arr[i-2] + arr[i-1];
+        }
+        return arr[n-1];
+    }
+}
