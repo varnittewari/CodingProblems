0e8a04486d198abf3eea4e1b669311f39d6f4a48
varnittewari <vxt6823@rit.edu>
2019-12-24 02:09:39 -0500

    ran problem 16

--- twitter16.java
+++ twitter16.java
@@ -1,7 +1,25 @@
+import java.util.LinkedList;
+
 public class twitter16{
 
+    static LinkedList<Integer> lis = new LinkedList<Integer>();
     public static void main (String[] args){
-
+        record(4);
+        record(1);
+        record(8);
+        record(6);
+        System.out.println(getLast(3));
+        record(10);
+        System.out.println(getLast(5));
+        record(7);
+        System.out.println(getLast(1));
     }
     
+    public static void record(int recId){
+        lis.add(recId);
+    }
+
+    public static int getLast(int i){
+        return lis.get(lis.size()-i);
+    }
 }
