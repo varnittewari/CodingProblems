8915abe290c6f168aad6e3d279e4d8cac434500d
Varnit-Tewari <varnit.tewari@ibm.com>
2020-02-10 14:42:06 -0500

    problem 25

--- facebook25.java
+++ facebook25.java
@@ -1,10 +1,10 @@
 public class facebook25{
 
-    public boolean wildcardMatching(String str, String pattern){
-        
+    public static boolean wildcardMatching(String str, String pattern){
+        return true;
     }
 
     public static void main(String args[]){
-
+        System.out.println(wildcardMatching("","*"));
     }
 }
